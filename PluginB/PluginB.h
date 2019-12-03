2b2d7807f0c5d9da5a6816127b95d1d14bd35c77
Ciprian Caba <ciprian.caba@adswizz.com>
2019-12-03 13:04:17 +0200

    First commit

---
+++ PluginB/PluginB.h
@@ -0,0 +1,19 @@
+//
+//  PluginB.h
+//  PluginB
+//
+//  Created by Ciprian Caba on 12/3/19.
+//  Copyright © 2019 AdsWizz, inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PluginB.
+FOUNDATION_EXPORT double PluginBVersionNumber;
+
+//! Project version string for PluginB.
+FOUNDATION_EXPORT const unsigned char PluginBVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PluginB/PublicHeader.h>
+
+
---
+++ PluginB/PluginB.swift
@@ -0,0 +1,9 @@
+//
+//  PluginB.swift
+//  PluginB
+//
+//  Created by Ciprian Caba on 12/3/19.
+//  Copyright © 2019 AdsWizz, inc. All rights reserved.
+//
+
+import Foundation
